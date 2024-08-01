49e8b9cc41c64a55a4f57ed873d07df33127d1f3
philcurtis422 <philcurtis422@gmail.com>
2024-07-31 20:17:51 -0400

    finished category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,21 +12,54 @@ router.get('/', async (req, res) => {
   res.json(categories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+	const id = req.params.id;
+
+	const category = await Category.findByPk(id, {
+		include: Product
+	});
+	res.json(category);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
+	const formData = req.body;
+	const category = await Category.create(formData);
+
+	res.json({
+		message: 'Category added successfully!',
+		category: category
+	});
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const category = await Category.update(
+	req.body,
+	{
+		where: {
+			id: req.params.id
+		} 
+	}
+  );
+  res.json(category[1]);
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
+	await Category.destroy({
+		where: {
+			id: req.params.id
+		}
+	});
+
+	res.json({
+		message: 'Category deleted successfully'
+	})
 });
 
 module.exports = router;
